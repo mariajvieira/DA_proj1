15f39d63a6b199de1829b5438d778b96542e8630
Duarte Marques <up202204973@up.pt>
2024-04-05 10:37:00 +0100

    1

--- src/BasicServiceMetrics.cpp
+++ src/BasicServiceMetrics.cpp
@@ -1,5 +1,17 @@
-//
-// Created by Duarte Marques on 05/04/2024.
-//
-
 #include "BasicServiceMetrics.h"
+
+void maxAmountWater(Graph<Node> &g, City c){
+
+    Node superSource = Node(0, "SuperSource");
+    Node superSink = Node(0, "SuperSink");
+    g.addVertex(superSource);
+    g.addVertex(superSink);
+    for(Vertex<Node> *v: g.getVertexSet()){
+        if(v->getInfo().getCode()[0]=='R') g.addPipe(superSource,v->getInfo(),INT_MAX);
+    }
+    for(Vertex<Node> *v: g.getVertexSet()){
+        if(v->getInfo().getCode()[0]=='C') g.addPipe(v->getInfo(),superSink,INT_MAX);
+    }
+
+}
+
--- src/BasicServiceMetrics.h
+++ src/BasicServiceMetrics.h
@@ -1,8 +1,12 @@
-//
-// Created by Duarte Marques on 05/04/2024.
-//
-
 #ifndef DA_PROJ1_BASICSERVICEMETRICS_H
 #define DA_PROJ1_BASICSERVICEMETRICS_H
+#include "Node.h"
+#include "Graph.h"
+#include "City.h"
+#include "Reservoir.h"
+#include "Station.h"
+
+
+void maxAmountWater(Graph<Node> &g, City c);
 
 #endif //DA_PROJ1_BASICSERVICEMETRICS_H
